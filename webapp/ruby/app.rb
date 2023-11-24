9d8d79923d50e9a4fbb66a74deb89270514060ca
nfphys <nfphys@gmail.com>
2023-11-24 04:15:05 +0000

    fix

--- webapp/ruby/app.rb
+++ webapp/ruby/app.rb
@@ -606,7 +606,10 @@ module Isuconquest
       db_transaction do
         # sessionを更新
         ################
-        user_id_store.write()
+        query = 'SELECT * from user_sessions where user_id=?'
+        session = db.xquery(query, json_params[:userId])[0]
+        user_id_store.write(session.id, nil)
+        expired_at_store.write(session.id, nil)
         query = 'UPDATE user_sessions SET deleted_at=? WHERE user_id=? AND deleted_at IS NULL'
         db.xquery(query, request_at, json_params[:userId])
         ################
