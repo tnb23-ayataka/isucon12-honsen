6816cb5d9a2c2605bd9308eda0579c3a26ac4f9c
otyamura <reiya-suzuki@c-fo.com>
2023-11-24 13:12:03 +0900

    Update schema with auto-increment and fix comments

--- webapp/sql/3_schema_exclude_user_presents.sql
+++ webapp/sql/3_schema_exclude_user_presents.sql
@@ -33,8 +33,8 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE `user_decks` (
-  `id` bigint NOT NULL,
-  `user_id` bigint NOT NULL comment 'ユーザID', 
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL comment 'ユーザID',
   `user_card_id_1` bigint NOT NULL comment '装備枠1',
   `user_card_id_2` bigint NOT NULL comment '装備枠2',
   `user_card_id_3` bigint NOT NULL comment '装備枠3',
@@ -46,8 +46,8 @@ CREATE TABLE `user_decks` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE `user_bans` (
-  `id` bigint NOT NULL,
-  `user_id` bigint NOT NULL comment 'ユーザID', 
+  `id` bigint NOT NULL  AUTO_INCREMENT,
+  `user_id` bigint NOT NULL comment 'ユーザID',
   `created_at` bigint NOT NULL,
   `updated_at`bigint NOT NULL,
   `deleted_at` bigint default NULL,
@@ -56,10 +56,10 @@ CREATE TABLE `user_bans` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE `user_devices` (
-  `id` bigint NOT NULL,
-  `user_id` bigint NOT NULL comment 'ユーザID', 
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL comment 'ユーザID',
   `platform_id` varchar(255) NOT NULL comment 'プラットフォームのviewer_id',
-  `platform_type` int(1) NOT NULL comment 'PC:1,iOS:2,Android:3', 
+  `platform_type` int(1) NOT NULL comment 'PC:1,iOS:2,Android:3',
   `created_at` bigint NOT NULL,
   `updated_at`bigint NOT NULL,
   `deleted_at` bigint default NULL,
@@ -72,7 +72,7 @@ CREATE TABLE `user_devices` (
 /* ログインボーナスマスタ */
 
 CREATE TABLE `login_bonus_masters` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `start_at` bigint NOT NULL comment '開始日時',
   `end_at` bigint comment '終了日時。Nullの場合、終了しない。',
   `column_count` int(2) NOT NULL comment '何日分用意するかの日数。例:7日のスタートダッシュ、20日の通常ログイン',
@@ -82,7 +82,7 @@ CREATE TABLE `login_bonus_masters` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE `login_bonus_reward_masters` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `login_bonus_id` bigint NOT NULL comment 'ログインボーナスID',
   `reward_sequence` int(2) NOT NULL comment '何日目の報酬か',
   `item_type` int(1) NOT NULL comment '付与するアイテム種別',
@@ -93,8 +93,8 @@ CREATE TABLE `login_bonus_reward_masters` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE `user_login_bonuses` (
-  `id` bigint NOT NULL,
-  `user_id` bigint NOT NULL comment 'ユーザID', 
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL comment 'ユーザID',
   `login_bonus_id` int NOT NULL comment 'ログインボーナスID',
   `last_reward_sequence` int NOT NULL comment '最終受け取り報酬番号',
   `loop_count` int NOT NULL comment 'ループ回数',
@@ -108,7 +108,7 @@ CREATE TABLE `user_login_bonuses` (
 /*  全員プレゼントマスタ */
 
 CREATE TABLE `present_all_masters` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `registered_start_at` bigint NOT NULL comment '配布対象のユーザの登録日時の起点日。この日付以降のユーザが対象',
   `registered_end_at` bigint NOT NULL  comment '配布対象のユーザの登録日時の終点日。この日付以前のユーザが対象',
   `item_type` int(1) NOT NULL comment 'アイテム種別',
@@ -122,7 +122,7 @@ CREATE TABLE `present_all_masters` (
 /* 全員プレゼント履歴テーブル */
 
 CREATE TABLE `user_present_all_received_history` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `user_id` bigint NOT NULL comment '受けとったユーザID',
   `present_all_id` bigint NOT NULL comment '全員プレゼントマスタのID',
   `received_at` bigint NOT NULL comment '受け取った日時',
@@ -135,7 +135,7 @@ CREATE TABLE `user_present_all_received_history` (
 /* ガチャマスタ */
 
 CREATE TABLE `gacha_masters` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `name` varchar(255) comment 'ガチャ名',
   `start_at` bigint NOT NULL comment '開始日時',
   `end_at` bigint NOT NULL comment '終了日時',
@@ -145,7 +145,7 @@ CREATE TABLE `gacha_masters` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE `gacha_item_masters` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `gacha_id` bigint NOT NULL comment 'ガチャ台のID',
   `item_type` int(1) NOT NULL comment 'アイテム種別',
   `item_id` int NOT NULL comment 'アイテムID',
@@ -157,7 +157,7 @@ CREATE TABLE `gacha_item_masters` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE `user_items` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `user_id` bigint NOT NULL comment 'ユーザID',
   `item_type` int(1) NOT NULL comment 'アイテム種別:1はusersテーブル、2はuser_cardsへ。3,4をこのテーブルへ保存',
   `item_id` int NOT NULL comment 'アイテムID',
@@ -170,7 +170,7 @@ CREATE TABLE `user_items` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE `user_cards` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `user_id` bigint NOT NULL comment 'ユーザID',
   `card_id` int NOT NULL comment '装備のID',
   `amount_per_sec` int NOT NULL comment '生産性（ISU/sec)',
@@ -186,7 +186,7 @@ CREATE TABLE `user_cards` (
 /*　アイテムマスタ、カードマスタ */
 
 CREATE TABLE `item_masters` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `item_type` int(2) NOT NULL comment '1:ISUCOIN、2:ハンマー（カード)、3:強化素材、4:時短アイテム（タイマー）',
   `name` varchar(128) NOT NULL comment 'アイテム名',
   `description` varchar(255) comment 'アイテム説明文',
@@ -203,14 +203,14 @@ CREATE TABLE `item_masters` (
 
 /*　マスタバージョンを管理するテーブル */
 CREATE TABLE `version_masters` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `status` int(2) NOT NULL comment 'ステータス 1: available、2:not_available',
   `master_version` varchar(128) NOT NULL comment 'マスタバージョン',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE `user_sessions` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `user_id` bigint NOT NULL,
   `session_id` varchar(128) NOT NULL,
   `created_at` bigint NOT NULL,
@@ -223,7 +223,7 @@ CREATE TABLE `user_sessions` (
 
 /* 更新処理について利用するone time tokenの管理 */
 CREATE TABLE `user_one_time_tokens` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `user_id` bigint NOT NULL,
   `token` varchar(128) NOT NULL,
   `token_type` int(2) NOT NULL comment '1:ガチャ用、2:カード強化用',
@@ -237,7 +237,7 @@ CREATE TABLE `user_one_time_tokens` (
 
 /* 管理者権限のセッション管理 */
 CREATE TABLE `admin_sessions` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `user_id` bigint NOT NULL,
   `session_id` varchar(128) NOT NULL,
   `created_at` bigint NOT NULL,
@@ -249,7 +249,7 @@ CREATE TABLE `admin_sessions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE `admin_users` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `password` varchar(255) NOT NULL,
   `last_activated_at` bigint NOT NULL comment '最終アクティブ日時',
   `created_at` bigint NOT NULL,
